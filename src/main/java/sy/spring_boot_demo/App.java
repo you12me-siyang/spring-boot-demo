70cec8deaf7e4da99a7ddcf4bf8d89df1f298fc3
tier <tier@ixiaopu.com>
2016-10-12 13:30:13 +0800

    first commit

---
+++ src/main/java/sy/spring_boot_demo/App.java
@@ -0,0 +1,13 @@
+package sy.spring_boot_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
